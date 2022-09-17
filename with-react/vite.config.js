8d238968d7ad72d846d4e4ed53eeb914ba055a90
mateonunez <mateonunez95@gmail.com>
2022-09-17 10:36:44 +0200

    chore(react): add rollup node polyfills

--- with-react/vite.config.js
+++ with-react/vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import Unocss from "unocss/vite";
+import rollupNodePolyFill from "rollup-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ["fs", "path"],
+  resolve: {
+    alias: {
+      path: "rollup-plugin-node-polyfills/polyfills/path",
     },
   },
-  plugins: [react(), Unocss()]
-})
+  build: {
+    rollupOptions: {
+      plugins: [rollupNodePolyFill()],
+    },
+  },
+  plugins: [react(), Unocss()],
+});
